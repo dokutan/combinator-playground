3e3439ae7ba38e5191a4649f0fc7f3a96894ee05
dokutan <54861821+dokutan@users.noreply.github.com>
2026-06-21 19:32:18 +0200

    Add a few test cases

--- test/combinator_playground/core_test.clj
+++ test/combinator_playground/core_test.clj
@@ -3,12 +3,20 @@
    [clojure.test :refer :all]
    [combinator-playground.combinators :refer [SKI]]
    [combinator-playground.core :refer :all]
-   [combinator-playground.reduce :refer [reduce*]]))
+   [combinator-playground.reduce :refer [reduce*]]
+   [combinator-playground.lambda :refer [lambda->SKI*]]))
 
 ;; (deftest a-test
 ;;   (testing "FIXME, I fail."
 ;;     (is (= 0 1))))
 
 (deftest SKI-test
-  (testing "SKI"
-    (is (= (last (reduce* SKI '(I x))) 'x))))
+  (is (= 'x     (last (reduce* SKI '(I x)))))
+  (is (= 'x     (last (reduce* SKI '(K x y)))))
+  (is (= '(x x) (last (reduce* SKI '(S I I x))))))
+
+(deftest lambda->SKI*-test
+  (is (= 'I       (last (lambda->SKI* '[x x]))))
+  (is (= 'K       (last (lambda->SKI* '[x [y x]]))))
+  (is (= '(K I)   (last (lambda->SKI* '[x [y y]]))))
+  (is (= '(S S I) (last (lambda->SKI* '[x [y x y (x y)]])))))
